150590d5664c4b5153346fa54dcf7f59dd8bad16
Chksonu700 <Chksonu700@gmail.com>
2022-01-26 07:52:19 +0100

    fix the bible link
    using axios without es6

--- bot.js
+++ bot.js
@@ -65,13 +65,19 @@ bot.onText(/\/bible/, (msg) => {
     
     const chatId = msg.chat.id;
     axios.get('https://labs.bible.org/api/?passage=random')
-    .then(response => {
-        const parsedHtml = parser(response.data);
+        .then(function (response) {
+        // handle success
+        console.log(response.data);
+        const parsedHtml = response.data;
         bot.sendMessage(chatId, parsedHtml, { parse_mode: 'HTML' });
-    }).catch(error => {
-        const errorText = `can't get Scripture Now <b>An error occured, please try again later</b>`;
-        bot.sendMessage(chatId, errorText, { parse_mode: 'HTML' })
-    });
+    })
+    .catch(function (error) {
+         // handle error
+         console.log(error);
+         const errorText = `can't get Scripture Now <b>An error occured, please try again later</b>`;
+         bot.sendMessage(chatId, errorText, { parse_mode: 'HTML' })
+    })
+
 });
 
 // Listen for any kind of message. There are different kinds of messages. 
